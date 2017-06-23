02c162cded70bda7d0784d0073cee07d4f1fb4b0
Vanitha <vanithaavani1996@gmail.com>
2017-06-23 11:31:57 +0530

    Create day.py

---
+++ day.py
@@ -0,0 +1,3 @@
+day={'monday':'working day','tuesday':'working day','wednesday':'working day','thursday':'working day','friday':'working day','saturday':'holiday','sunday':'holiday'}
+d=input("Enter day")
+print(day[d])
